d25bf2a7bed66f0e5988d3633264d0e279a62d82
Bohdan <blazarchuk@gmail.com>
2025-09-24 17:05:36 +0300

    Update Convector - add users enter kg

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,19 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Converter App.");
+        System.out.println("Будь ласка введіть кількість кілограм");
 
-        double kgs = 2.7;
+        double kgs = scanner.nextDouble();
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Resoult is " + kgs + " kgs equals " + pounds + " pounds");
+        System.out.println("Result is " + kgs + " kgs equals " + pounds + " pounds");
     }
 
     private static double convKgsToPounds(double kgs){
