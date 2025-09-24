49b6d5d95de27baf46d1d5d5a52c9eae0a78f1b4
Bohdan <blazarchuk@gmail.com>
2025-09-24 17:57:49 +0300

    Update Convector - add pounds to kgs

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,14 +9,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Converter App.");
-        System.out.println("Будь ласка введіть кількість кілограм");
 
+        System.out.print("Please enter kilos: ");
         double kgs = scanner.nextDouble();
+
+        System.out.print("Please enter pounds: ");
+        double pnds = scanner.nextDouble();
+
         double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+
         System.out.println("Result is " + kgs + " kgs equals " + pounds + " pounds");
+        System.out.println("Result is " + pnds + " pnds equals " + kilos + " kgs");
     }
 
     private static double convKgsToPounds(double kgs){
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds){
+        return pnds / CONV_K;
     }
 }
